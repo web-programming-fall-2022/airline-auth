52d7677a68110165db00306794666529bc48d5a9
Pouya Esmaili <poesmaili@gmail.com>
2023-02-11 22:33:15 +0330

    Solve cors

--- internal/server/run_server.go
+++ internal/server/run_server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/redis/go-redis/v9"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"github.com/web-programming-fall-2022/airline-auth/internal/bootstrap"
 	"github.com/web-programming-fall-2022/airline-auth/internal/bootstrap/job"
 	"github.com/web-programming-fall-2022/airline-auth/internal/cfg"
@@ -14,8 +15,8 @@ import (
 	pb "github.com/web-programming-fall-2022/airline-auth/pkg/api/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"log"
 	"net/http"
+	"regexp"
 )
 
 func RunServer(ctx context.Context, config cfg.Config) job.WithGracefulShutdown {
@@ -34,9 +35,9 @@ func RunServer(ctx context.Context, config cfg.Config) job.WithGracefulShutdown 
 
 	store := storage.NewStorage(&config.MainDB)
 
-	if err := store.Migrate(); err != nil {
-		log.Fatal(err)
-	}
+	//if err := store.Migrate(); err != nil {
+	//	log.Fatal(err)
+	//}
 
 	tokenManager := token.NewJWTManager(config.JWT.Secret, store, rdb)
 
@@ -82,7 +83,7 @@ func RunHttpServer(ctx context.Context, config cfg.Config) job.WithGracefulShutd
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", config.HttpServer.Port),
-		Handler: mux,
+		Handler: cors(mux),
 	}
 
 	logrus.Info("Starting HTTP/REST Gateway...", srv.Addr)
@@ -93,4 +94,28 @@ func RunHttpServer(ctx context.Context, config cfg.Config) job.WithGracefulShutd
 		}
 	}()
 	return srv
+}
+
+func allowedOrigin(origin string) bool {
+	if viper.GetString("cors") == "*" {
+		return true
+	}
+	if matched, _ := regexp.MatchString(viper.GetString("cors"), origin); matched {
+		return true
+	}
+	return false
+}
+
+func cors(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if allowedOrigin(r.Header.Get("Origin")) {
+			w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, ResponseType")
+		}
+		if r.Method == "OPTIONS" {
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
 }
